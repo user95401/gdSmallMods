a40ac6a5cc209bc14c7c309d549bf38a186701bf
user95401 <user95401@gmail.com>
2023-08-30 23:45:01 +0400

    231

--- src/FUHD_SCALE_FACTOR.cpp
+++ src/FUHD_SCALE_FACTOR.cpp
@@ -10,40 +10,77 @@ using namespace cocos2d;
 using namespace gd;
 using namespace cocos2d::extension;
 
-class MenuLayerMod {
+class HttpRequests {
+public:
     void onUpdateHttpResponse(CCHttpClient* client, CCHttpResponse* response) {
         std::vector<char>* responseData = response->getResponseData();
         std::string responseString(responseData->begin(), responseData->end());
-        if (responseString != version) {
+        if (std::string(CCFileUtils::sharedFileUtils()->fullPathForFilename("CCControlColourPickerSpriteSheet-uhd.png", true)).find("FUHD_" + responseString) == std::string::npos) {
+            if (
+                MessageBoxA(nullptr, 
+                    (
+                    "Something wrong with resources!\n"
+                    "it maybe outdated or was not loaded.\n"
+                    ""
+                    "The \"FUHD_"+responseString+"\" was not founded in path:\n"+ 
+                    CCFileUtils::sharedFileUtils()->fullPathForFilename("CCControlColourPickerSpriteSheet-uhd.png", true) +
+                    "\n\nDownload actual resource pack?"
+                    ).c_str(), 
+                "FUHD_SCALE_FACTOR MOD", MB_ICONERROR | MB_YESNO)
+                == IDYES
+                ) CCApplication::sharedApplication()->openURL("http://user95401.webq2e.ru/FUHD_SCALE_FACTOR_LAST_TP_VER.download");
         }
     }
 };
 
-inline MenuLayer* (__cdecl* MenuLayer_init)(MenuLayer*);
-bool __fastcall MenuLayer_init_H(MenuLayer* self) {
-    if (!MenuLayer_init(self)) return false;
+const char* (__thiscall* LoadingLayer_getString)(LoadingLayer* self);
+const char* __fastcall LoadingLayer_getString_H(LoadingLayer* self, void*) {
+    //CCDirector::sharedDirector()->setContentScaleFactor(8.f); fucks "GJ_GameSheet.plist"
+    if(CC_CONTENT_SCALE_FACTOR() > 4.f) return LoadingLayer_getString(self);
+    else return std::string(
+        "\n"
+        "GD FUHD\n\nBy user666").c_str();
+}
+
+inline LoadingLayer* (__cdecl* loadAssets)(LoadingLayer*);
+void __fastcall loadAssets_H(LoadingLayer* self, void*) {
+    loadAssets(self);
+    auto parent = self->getParent();
+    if (self->m_nLoadIndex == 1 and parent->getTag() != 21356) self->addChild(self->create(self->m_bFromRefresh), 100, 21356);
+    CCDirector::sharedDirector()->setContentScaleFactor(8.f);
+
+    CCLabelBMFont* warn;
+
+    if (self->m_nLoadIndex <= 1) {//one time creating nodes
+        warn = CCLabelBMFont::create("One more time reloading contains bugs!", "bigFont.fnt");
+        warn->setAnchorPoint({ -0.005 , 0.000 });
+        warn->setScale(0.450);
+        warn->setColor(ccRED);
+    }
+
+    if (self->m_bFromRefresh) {
+        if(!self->getChildByTag(5957)) self->addChild(warn, 5, 5957);
+    }
+}
+
+inline LoadingLayer* (__cdecl* loadingFinished)(LoadingLayer*);
+void __fastcall loadingFinished_H(LoadingLayer* self) {
+    loadingFinished(self);
 
     //udate if sddso herer
     CCHttpRequest* request = new CCHttpRequest;
     request->setRequestType(CCHttpRequest::HttpRequestType::kHttpPost);
-    request->setUrl(MenuLayerMod::upadateInfoUrl);
-    request->setResponseCallback(self, httpresponse_selector(MenuLayerMod::onUpdateHttpResponse));
+    request->setUrl("http://user95401.webq2e.ru/FUHD_SCALE_FACTOR_LAST_TP_VER.inf");
+    request->setResponseCallback(self, httpresponse_selector(HttpRequests::onUpdateHttpResponse));
     CCHttpClient::getInstance()->send(request);
     request->release();
-
-    return true;
-}
-
-
-inline void(__thiscall* GameManager_update)(GameManager*, float);
-void __fastcall GameManager_update_H(GameManager* self, void*, float dt) {
-    GameManager_update(self, dt);
-    CCDirector::sharedDirector()->setContentScaleFactor(8.f);
 }
 
 DWORD WINAPI thread_func(void* hModule) {
     MH_Initialize();
-    HOOK(base + 0xce440, GameManager_update, true);
+    HOOK(base + 0x18cf40, LoadingLayer_getString, true);
+    HOOK(base + 0x18C8E0, loadAssets, true);
+    HOOK(base + 0x18c790, loadingFinished, false);
     MH_EnableHook(MH_ALL_HOOKS);
     return 0;
 }
