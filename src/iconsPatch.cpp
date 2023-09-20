7ac0588bc22de9466d64072c576698295b2262f9
user95401 <user95401@gmail.com>
2023-09-20 22:28:30 +0400

    unnamed

--- src/iconsPatch.cpp
+++ src/iconsPatch.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 #include "private_code/iconsPatch.hpp"
 
+#include "SimpleIni.h"
+
 inline void(__thiscall* update)(GameManager* self, float);//0xce440
 void __fastcall update_H(GameManager* self, void*, float dt) {
     update(self, dt);
@@ -14,21 +16,24 @@ void __fastcall update_H(GameManager* self, void*, float dt) {
 }
 
 DWORD WINAPI iconsPatch(void* hModule) {
+    CSimpleIni ini;
+    filesystem::create_directories("config");
     ini.LoadFile("config/iconsPatch.ini");
-    if (!ini.GetValue("iconsPatch", "cube")) {
-        filesystem::create_directories("config");
-        std::ofstream ("config/iconsPatch.ini");
-        ini.SetValue("DISCOVERED BY RUHAX | GUSCHIN", "", "");
-        ini.DeleteValue("DISCOVERED BY RUHAX | GUSCHIN", "", "", false);
-        ini.SetValue("iconsPatch", "cube", "142", "; org count of cubes 142");
+    if (!(ini.KeyExists("iconsPatch", "cube")))
+        ini.SetValue("iconsPatch", "cube", "142", ";------------------------\n; DISCOVERED BY GUSCHIN\n;------------------------\n; org count of cubes 142");
+    if (!(ini.KeyExists("iconsPatch", "ship")))
         ini.SetValue("iconsPatch", "ship", "51", "; org count of ships 51");
+    if (!(ini.KeyExists("iconsPatch", "ball")))
         ini.SetValue("iconsPatch", "ball", "42", "; org count of BALLS 42");
+    if (!(ini.KeyExists("iconsPatch", "ufo")))
         ini.SetValue("iconsPatch", "ufo", "35", "; org count of ufos 35");
+    if (!(ini.KeyExists("iconsPatch", "wave")))
         ini.SetValue("iconsPatch", "wave", "35", "; org count of waves 35");
+    if (!(ini.KeyExists("iconsPatch", "robot")))
         ini.SetValue("iconsPatch", "robot", "26", "; org count of robots 26");
+    if (!(ini.KeyExists("iconsPatch", "spider")))
         ini.SetValue("iconsPatch", "spider", "17", "; org count of robots 17");
-        ini.SaveFile("config/iconsPatch.ini");
-    }
+    ini.SaveFile("config/iconsPatch.ini");
     MH_SafeInitialize();
     HOOK(base + 0xce440, update);
     MH_EnableHook(MH_ALL_HOOKS);
--- src/private_code/iconsPatch.hpp
+++ src/private_code/iconsPatch.hpp
@@ -1,6 +1,1 @@
-#include "mod_utils.hpp"
-#include "SimpleIni.h"
-using namespace std;
-using namespace gd;
-static CSimpleIni ini;
 void updateIconsCount();
